38d2dc0d32fb3eeb9187e8f6eebec28441559d8b
primetime43 <12754111+primetime43@users.noreply.github.com>
2023-07-02 23:36:07 -0400

    Should fix issue #3
    - Fixes issue #3 by hosting not only on local loopback address.

--- GAPS 2.py
+++ GAPS 2.py
@@ -324,4 +324,4 @@ currentActiveServer = PlexAccountData()
 moviesFromSelectedLibrary = {}
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(host='0.0.0.0', port=5000, debug=False)
