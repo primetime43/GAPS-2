0485642882dd3340d8d6ecf764c24bc70422024c
primetime43 <12754111+primetime43@users.noreply.github.com>
2026-03-10 23:46:23 -0400

    Improve Docker deployment
    Improve Docker deployment with gunicorn, persistent volumes, and .dockerignore

--- backend/run.py
+++ backend/run.py
@@ -1,6 +1,8 @@
+import os
 from app import create_app
 
-app = create_app()
+config = 'production' if os.environ.get('FLASK_ENV') == 'production' else None
+app = create_app(config)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
